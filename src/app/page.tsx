169bd454fb9b38abad57f41eace5968c7e4be4c3
marc7hedev <driberstagg@gmail.com>
2024-07-31 17:56:06 -0600

    Añadidos más charts y acomodado los cards principales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { AreaChartComponent } from "@/components/AreaChart";
 import { CardUsageExample } from "@/components/CardStat";
+import { DonutChartExample } from "@/components/DonutChar";
 import FormExample from "@/components/FormExample";
 import { ListUsageExample } from "@/components/ListExample";
 import { TableExample } from "@/components/TableExample";
@@ -11,17 +12,18 @@ function page(){
     <div className="container mx-auto pt-10">
 
       <div className="grid grid-cols-4 gap-2">
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
-        <CardUsageExample />
+        <div className="col-span-2 grid grid-cols-2">
+          <CardUsageExample />
+          <CardUsageExample />
+        </div>
+        <div className="col-span-2">
+          <Card>
+            <DonutChartExample />
+          </Card>
+        </div>
       </div>
 
-      <div className="grid grid-cols-4 mt-10">
+      <div className="grid grid-cols-4 mt-5">
         <div className="col-span-2 px-7">
           <ListUsageExample />
           <div className="pt-5">
