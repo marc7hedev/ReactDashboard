ca650316c77e0a669230cdefea94e5627d3259f1
marc7hedev <driberstagg@gmail.com>
2024-07-31 19:43:20 -0600

    Implementadas clases de tailwind para mejorar adaptabilidad a mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,30 @@ import { Card } from "@tremor/react"
 function page(){
   return (
     
-    <div className="container mx-auto pt-10">
+    <div className="container mx-auto pt-10 px-4 md:px-8">
 
-      <div className="grid grid-cols-4 gap-2">
-        <div className="col-span-2 grid grid-cols-2 gap-4 px-7">
+      {/* <div className="grid grid-cols-4 gap-2"> */}
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        {/* <div className="col-span-2 grid grid-cols-2 gap-4 px-7"> */}
+        <div className="md:col-span-2 grid grid-cols-2 md:grid-cols-2 gap-4">
           <CardUsageExample />
           <CardUsageExample />
         </div>
-        <div className="col-span-2 px-7">
+        <div className="md:col-span-2">
           <Card>
             <DonutChartExample />
           </Card>
         </div>
       </div>
 
-      <div className="grid grid-cols-4 mt-5">
-        <div className="col-span-2 px-7">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-5">
+        <div className="md:col-span-2">
           <ListUsageExample />
           <div className="pt-5">
           <TableExample/>
           </div>
         </div>
-        <div className="col-span-2 px-7">
+        <div className="md:col-span-2">
           <Card>
             <AreaChartComponent />
           </Card>
